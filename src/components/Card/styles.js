9f5d58d621622a9137b21d567832cd773350d092
VictorGAG <tblv@Outlook.com.br>
2020-06-24 20:17:27 -0300

    Added Header and Board

---
+++ src/components/Card/styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  
+    img {
+        width: 32px;
+        height: 32px;
+        border-radius: 50px;
+        margin-top: 5px;
+    }
+
+`;
+
+export const Label = styled.span`
+
+`;
