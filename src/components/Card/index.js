cb3a2f88f39872d982a65691ff6f672910c238ff
VictorGAG <tblv@Outlook.com.br>
2020-06-24 23:22:26 -0300

    Finalized DragnDrop

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,13 +1,16 @@
-import React, { useRef } from 'react';
+import React, { useRef, useContext } from 'react';
 import { useDrag, useDrop } from 'react-dnd';
 import { Container, Label } from './styles';
 
-export default function Card({data, index}) {
+import BoardContext from '../Board/context';
+
+export default function Card({data, index, listIndex}) {
 
   const ref = useRef();
+  const { move } = useContext(BoardContext);
 
   const [{ isDragging }, dragRef] = useDrag({
-    item: { type: 'CARD', index},
+    item: { type: 'CARD', index, listIndex},
     collect: monitor => ({
       isDragging: monitor.isDragging(),
     }),
@@ -17,19 +20,22 @@ export default function Card({data, index}) {
   const [, dropRef] = useDrop({
     accept: 'CARD',
     hover(item, monitor){
+      const draggedListIndex = item.listIndex;
+      const targetListIndex = listIndex;
+
       const draggedIndex = item.index;
       const targetIndex = index;
-      
+
+      if (draggedIndex === targetIndex && draggedIndex === targetListIndex){
+        return;
+      }
+
       const targetSize = ref.current.getBoundingClientRect();
       const targetCenter = (targetSize.bottom - targetSize.top) / 2;
       
       const draggedOffset = monitor.getClientOffset();
       const draggedTop = draggedOffset.y - targetSize.top;
 
-      if (draggedIndex === targetIndex){
-        return;
-      }
-      
       if (draggedIndex < targetIndex && draggedTop < targetCenter){
         return;
       }
@@ -38,7 +44,10 @@ export default function Card({data, index}) {
         return;
       }
 
-      console.log("teste");
+      move(draggedListIndex, targetListIndex, draggedIndex, targetIndex);
+      
+      item.index = targetIndex;
+      item.listIndex = targetListIndex;
     }
   });
 
